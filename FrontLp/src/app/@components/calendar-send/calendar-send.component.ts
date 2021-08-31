ce100113276091359f0d867361a4708d7dc769e6
petdela <petdela@espol.edu.ec>
2021-08-30 19:38:46 -0500

    creacion de carpeta para componentes

---
+++ FrontLp/src/app/@components/calendar-send/calendar-send.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar-send',
+  templateUrl: './calendar-send.component.html',
+  styleUrls: ['./calendar-send.component.css']
+})
+export class CalendarSendComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ FrontLp/src/app/@components/calendar-send/calendar-send.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CalendarSendComponent } from './calendar-send.component';
+
+
+
+@NgModule({
+  declarations: [CalendarSendComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CalendarSendModule { }
