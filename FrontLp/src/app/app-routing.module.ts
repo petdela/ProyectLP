01cdf7f6947c850778bb4794344b4aa167ed66a3
Sebastian <samendoz@espol.edu.ec>
2021-08-30 22:35:15 -0500

    asd

--- FrontLp/src/app/app-routing.module.ts
+++ FrontLp/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EventosAdminComponent } from './@components/eventos-admin/eventos-admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'eventos', component: EventosAdminComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FrontLp/src/app/app.module.ts
+++ FrontLp/src/app/app.module.ts
@@ -4,15 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalendarSendModule } from './@components/calendar-send/calendar-send.module';
-
+import { EventosAdminModule } from './@components/eventos-admin/eventos-admin.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CalendarSendModule,
+    EventosAdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
