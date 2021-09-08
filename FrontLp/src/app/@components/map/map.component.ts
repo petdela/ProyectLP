c6b2945440dd0044f0036c00100c75f9b1f9bdc3
Robert Moreno Carrillo <rodemore@espol.edu.ec>
2021-09-07 22:48:30 -0500

    Update map component

--- FrontLp/src/app/@components/map/map.component.ts
+++ FrontLp/src/app/@components/map/map.component.ts
@@ -11,6 +11,17 @@ export class MapComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var myIcon = L.icon({
+      iconUrl: 'http://pngimg.com/uploads/gps/gps_PNG8.png',
+      shadowUrl: 'http://pngimg.com/uploads/gps/gps_PNG8.png',
+
+      iconSize:     [50, 64], // size of the icon
+      shadowSize:   [0, 0], // size of the shadow
+      iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+      shadowAnchor: [0, 0],  // the same for the shadow
+      popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+  });
+
 
     let mymap = L.map('mapid').setView([-2.203816, -79.897453], 10);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoicm9kZW1vcmUiLCJhIjoiY2t0MHYzYjlhMDkwdDJvbm1kMWR3ZWVzMiJ9.STQ27_RMlKmIRMmGFrP-GA', {
@@ -20,33 +31,32 @@ export class MapComponent implements OnInit {
       tileSize: 512,
       zoomOffset: -1,
       accessToken: 'your.mapbox.access.token'
-  }).addTo(mymap);
+  }).addTo(mymap);    
+  
 
-  var marker = L.marker([-2.180061, -79.9053203]).addTo(mymap);
-  marker.bindPopup(`
-  <div class="card" style="width: 18rem;">
-    <div class="card-body">
-      <h3 class="card-title"><b>GYE TECH: Clojure, la ciencia de lo simple.</b></h3>
-      <h6 class="card-subtitle"><b>Tecnología</b> - 5/09/2021</h6>
-      <p class="card-text">Tercera charla de la iniciativa Guayaquil tech. Una oportunidad de conocer nuevas tecnologías de de mejorar la red de contactos conectando y convocando a grandes desarrolladores de la ciudad de Guayaquil</p>
-      <a href="#" class="btn btn-success">Agregar al calendario</a>
-    </div>
-  </div>
-  `).openPopup();
-
-
-  var marker = L.marker([-2.1552202, -79.8948806]).addTo(mymap);
-  marker.bindPopup(`
-  <div class="card" style="width: 18rem;">
-    <div class="card-body">
-      <h3 class="card-title"><b>Fiesta destructiva en casa de Robert</b></h3>
-      <h6 class="card-subtitle"><b>Entretenimiento</b> - 5/09/2021</h6>
-      <p class="card-text">Cansado y estresado por el semestre? <br> Te fue bien en los examenes? Ven y celebra con nosotros <br> Te fue mal en los examenes? Ven y llora con nosotros.  </p>
-      <a href="#" class="btn btn-success">Agregar a mi calendario</a>
-    </div>
-  </div>
-  `).openPopup();
-
+  fetch(`http://localhost:8000/get_eventos`)
+  .then(res => res.json())
+  .then(res => {
+    for (let i = 0; i < res.length; i++) {
+      let startIndx = res[i].ubicacion.search("@");
+      let commaIndx = startIndx + res[i].ubicacion.slice(startIndx, -1).search(",", startIndx+1);
+      let latlong = res[i].ubicacion.slice(startIndx+1, commaIndx+res[i].ubicacion.slice(commaIndx+2,-1).search(",",commaIndx+2))
+      let latlongList = latlong.split(",").map(parseFloat)
+      var marker = L.marker(latlongList, {icon: myIcon}).addTo(mymap);
+      
+     marker.bindPopup(`
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h3 class="card-title"><b>${res[i].nombre}</b></h3>
+          <h6 class="card-subtitle"><b>${res[i].categoria}</b> - 5/09/2021</h6>
+          <p class="card-text">${res[i].descripcion} </p>
+          <a href="#" class="btn btn-success">Agregar a mi calendario</a>
+        </div>
+      </div>
+    `).openPopup();
+   }
+    
+  });
   
   }
 
--- FrontLp/src/app/app-routing.module.ts
+++ FrontLp/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EventosAdminComponent } from './@components/eventos-admin/eventos-admin.component';
 import { EventosUpdateComponent } from './@components/eventos-update/eventos-update.component';
-
+import {MapComponent} from './@components/map/map.component'
 const routes: Routes = [
   {path: 'eventos/save', component: EventosAdminComponent},
-  {path: 'eventos/update', component: EventosUpdateComponent}
+  {path: 'eventos/update', component: EventosUpdateComponent},
+  {path: '', component: MapComponent}
 ];
 
 @NgModule({
